c576cf1ffd8632546a5a1d71ba2a8b7bec60e300
AK44479 <AK44479@users.noreply.github.com>
2021-02-19 23:12:56 -0500

    Added Three.js into html
    I got the three.js working in Jason #app canvas and it responsive to window with. I have comment the functions to help further understand the main js three js codes.

--- src/main.js
+++ src/main.js
@@ -7,3 +7,91 @@ const init = () => {
 window.onload = () => { 
   init(); 
 };
+
+/*++++++++++Three.js++++++++++*/
+//Connected the canvas to the #app html canvas Jason set up on
+const canvas = document.querySelector('#app');
+//Renders the 3.js on to the canvas
+const renderer = new THREE.WebGLRenderer({canvas});
+//Appends to html
+document.body.appendChild( renderer.domElement );
+
+//Setting up the scene for the 3D to be in 
+const fov = 75;
+const aspect = 2;  // the canvas default
+const near = 0.1;
+const far = 5;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+//Setting the 3D stuff 
+const scene = new THREE.Scene();
+
+{
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+}
+
+//Creating box shape
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+function makeInstance(geometry, color, x) {
+  const material = new THREE.MeshPhongMaterial({color});
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  cube.position.x = x;
+
+  return cube;
+}
+
+//Creates different cubes
+const cubes = [
+  makeInstance(geometry, 0x44aa88,  0),
+  makeInstance(geometry, 0x8844aa, -2),
+  makeInstance(geometry, 0xaa8844,  2),
+];
+
+
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+//Important function This helps update refresh frame rate and keeping canvas width responsive
+function render(time) {
+  time *= 0.001;
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  cubes.forEach((cube, ndx) => {
+    const speed = 1 + ndx * .1;
+    const rot = time * speed;
+    cube.rotation.x = rot;
+    cube.rotation.y = rot;
+  });
+
+  //Renders the things out to html canvas
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
