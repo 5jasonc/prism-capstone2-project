878a164438852186c98f8963e0a89bcb30ddf38e
5jasonc <jason.c.streeter@gmail.com>
2021-03-05 14:27:13 -0500

    Improved jelly generation function. Got rid of Alex's cube demo, main page now generates 5 random jellies and allows you to type in a wish and generate new jellies.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 // Holds all cubes/jellyfish
 const cubes = [];
 const jellies = [];
-let a = 0;
 
 // Kick off program
 const init = () => {
@@ -28,19 +27,28 @@ const init = () => {
   document.body.appendChild(renderer.domElement);
 
   // Create camera and add to scene
-  let camZoom = 130;
+  let camZoom = 1000;
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(60, window.innerWidth  / window.innerHeight, 0.1, 20000);
-  camera.position.set(0, 0, camZoom);
+  camera.position.set(500, 500, camZoom);
   scene.add(camera);
 
+  // Set up orbit camera controls
+  const controls = new THREE.OrbitControls(camera, renderer.domElement)
+	controls.listenToKeyEvents( window ); // optional
+
   // Listen for change in zoom slider
-  document.querySelector('#myCamRange').addEventListener('input', e => {
-    camZoom = e.target.value;
-    camera.position.set(0, 0, camZoom);
+  // document.querySelector('#myCamRange').addEventListener('input', e => {
+  //   camZoom = e.target.value;
+  //   camera.position.set(0, 0, camZoom);
+  // });
+
+  // Listen for make wish button being clicked, generate a new jellyfish with current wish input
+  document.querySelector('#makeWishButton').addEventListener('click', () => {
+    generateJelly(document.querySelector('#wishInput').value, scene);
   });
 
-  // Create lights and add to scene
+  // Create light and add to scene
   const light = new THREE.PointLight(0xfffff, 1, 500);
   light.position.set(10, 0, 25);
   scene.add(light)
@@ -53,37 +61,39 @@ const init = () => {
   });
 
   // Specify cube shape and material
-  const geometry = new THREE.BoxGeometry(5, 10, 5);
-  const material = new THREE.MeshNormalMaterial();
+  // const geometry = new THREE.BoxGeometry(5, 10, 5);
+  // const material = new THREE.MeshNormalMaterial();
 
   // Add 500 cubes to scene
-  for (let x = 0; x < 500; x++) {
-    const cube = new THREE.Mesh(geometry, material);
+  // for (let x = 0; x < 500; x++) {
+  //   const cube = new THREE.Mesh(geometry, material);
 
-    cube.position.x = Math.random() * 75 - 50;
-    cube.position.y = Math.random() * 75 - 50;
-    cube.position.z = 0;
+  //   cube.position.x = Math.random() * 75 - 50;
+  //   cube.position.y = Math.random() * 75 - 50;
+  //   cube.position.z = 0;
 
-    cube.rotation.x = Math.random() * 2 * Math.PI;
-    cube.rotation.y = Math.random() * 2 * Math.PI;
-    cube.rotation.z = Math.random() * 2 * Math.PI;
+  //   cube.rotation.x = Math.random() * 2 * Math.PI;
+  //   cube.rotation.y = Math.random() * 2 * Math.PI;
+  //   cube.rotation.z = Math.random() * 2 * Math.PI;
 
-    cube.scale.x = Math.random() + 0.5;
-    cube.scale.y = Math.random() + 0.5;
-    cube.scale.z = Math.random() + 0.5;
+  //   cube.scale.x = Math.random() + 0.5;
+  //   cube.scale.y = Math.random() + 0.5;
+  //   cube.scale.z = Math.random() + 0.5;
 
-    cube.userData.velocity = new THREE.Vector3();
-    cube.userData.velocity.x = Math.random() * 0.4 - 0.2;
-    cube.userData.velocity.y = Math.random() * 0.4 - 0.2;
-    cube.userData.velocity.z = Math.random() * 0.4 - 0.2;
+  //   cube.userData.velocity = new THREE.Vector3();
+  //   cube.userData.velocity.x = Math.random() * 0.4 - 0.2;
+  //   cube.userData.velocity.y = Math.random() * 0.4 - 0.2;
+  //   cube.userData.velocity.z = Math.random() * 0.4 - 0.2;
   
-    scene.add(cube);
-    cubes.push(cube);
+  //   scene.add(cube);
+  //   cubes.push(cube);
+  // }
+
+  // Add 5 random jellies to the screen
+  for(let i = 0; i < 5; i++) {
+    generateJelly(`Jelly-${randomNum(-1000, 1000)}`, scene);
   }
-
-  // generateJelly('test', scene);
-  // generateJelly('test2222222', scene);
-
+  
   // Render scene
   renderer.render(scene, camera);
 
@@ -91,74 +101,109 @@ const init = () => {
   animate(renderer, scene, camera);
 };
 
-
 // Loops through to animate
 const animate = (renderer, scene, camera) => {
   requestAnimationFrame(() => animate(renderer, scene, camera));
 
   // Make cubes move around
-  for (let i = 0; i < cubes.length; i++) {
-    const cube = cubes[i];
-    cube.position.add(cube.userData.velocity);
-    if (cube.position.x < -100 || cube.position.x > 100) {
-      cube.position.x = THREE.Math.clamp(cube.position.x, -100, 100);
-      cube.userData.velocity.x = -cube.userData.velocity.x;
+  // for (let i = 0; i < cubes.length; i++) {
+  //   const cube = cubes[i];
+  //   cube.position.add(cube.userData.velocity);
+  //   if (cube.position.x < -100 || cube.position.x > 100) {
+  //     cube.position.x = THREE.Math.clamp(cube.position.x, -100, 100);
+  //     cube.userData.velocity.x = -cube.userData.velocity.x;
+  //   }
+  //   if (cube.position.y < -100 || cube.position.y > 100) {
+  //     cube.position.y = THREE.Math.clamp(cube.position.y, -100, 100);
+  //     cube.userData.velocity.y = -cube.userData.velocity.y;
+  //   }
+  //   if (cube.position.z < -100 || cube.position.z > 100) {
+  //     cube.position.z = THREE.Math.clamp(cube.position.z, -100, 100);
+  //     cube.userData.velocity.z = -cube.userData.velocity.z;
+  //   }
+  //   cube.rotation.x += 0.01;
+  // }
+
+  // Make jellies move around, make them turn around if they hit walls
+  for(let i = 0; i < jellies.length; i++) {
+    const jelly = jellies[i].jellyParent;
+    jelly.translateY(jellies[i].aStep * 2 + 0.3);
+    if(jelly.position.x < -300 || jelly.position.x > 300) {
+      jelly.position.x = THREE.Math.clamp(jelly.position.x, -300, 300);
+      jelly.rotateX(Math.PI);
     }
-    if (cube.position.y < -100 || cube.position.y > 100) {
-      cube.position.y = THREE.Math.clamp(cube.position.y, -100, 100);
-      cube.userData.velocity.y = -cube.userData.velocity.y;
+    if(jelly.position.y < -300 || jelly.position.y > 300) {
+      jelly.position.y = THREE.Math.clamp(jelly.position.y, -300, 300);
+      jelly.rotateY(Math.PI);
     }
-    if (cube.position.z < -100 || cube.position.z > 100) {
-      cube.position.z = THREE.Math.clamp(cube.position.z, -100, 100);
-      cube.userData.velocity.z = -cube.userData.velocity.z;
+    if(jelly.position.z < -300 || jelly.position.z > 300) {
+      jelly.position.z = THREE.Math.clamp(jelly.position.z, -300, 300);
+      jelly.rotateZ(Math.PI);
     }
-    cube.rotation.x += 0.01;
   }
 
   // Make jellies pulsate
-  // for(let j = 0; j < jellies.length; j++) {
-  //   const position = jellies[j].geometry.attributes.position;
-  //   const vector = new THREE.Vector3();
+  for(let j = 0; j < jellies.length; j++) {
+    const position = jellies[j].jelly.geometry.attributes.position;
+    const vector = new THREE.Vector3();
 
-  //   for ( let i = 0; i < position.count; i++ ){
-  //       // console.log("yes");
-  //       vector.fromBufferAttribute( position, i );
-  //       vector.applyMatrix4( jellies[j].matrixWorld );
-  //       let size = 12;
-  //       let magnitude = 7;
-  //       let dist = new THREE.Vector3(vector.x, vector.y, vector.z).sub(new THREE.Vector3(0, 1000, 0));
+    for(let i = 0; i < position.count; i++){
+        vector.fromBufferAttribute(position, i);
+        vector.applyMatrix4(jellies[j].jelly.matrix);
+        let size = 12;
+        let magnitude = 7;
+        let dist = new THREE.Vector3(vector.x, vector.y, vector.z).sub(new THREE.Vector3(0, 1000, 0));
 
-  //       vector.normalize();
-  //       vector.multiplyScalar(40 + Math.sin(dist.length()/-size + a) * magnitude);
-  //       // position.setXYZ(i, vector.x, vector.y, vector.z);
-  //       position.setXYZ(i, vector.x, vector.y, vector.z);
+        vector.normalize();
+        vector.multiplyScalar(40 + Math.sin(dist.length() / -size + jellies[j].a) * magnitude);
+        position.setXYZ(i, vector.x, vector.y, vector.z);
 
-  //   }
-  //   position.needsUpdate = true;
-  //   jellies[j].geometry.verticesNeedUpdate = true;
-  //   jellies[j].geometry.normalsNeedUpdate = true;
-  //   jellies[j].geometry.computeVertexNormals();
-  //   jellies[j].geometry.computeFaceNormals();
-  //   a += 0.02;
-  // }
+    }
+    position.needsUpdate = true;
+    jellies[j].jelly.geometry.verticesNeedUpdate = true;
+    jellies[j].jelly.geometry.normalsNeedUpdate = true;
+    jellies[j].jelly.geometry.computeVertexNormals();
+    jellies[j].jelly.geometry.computeFaceNormals();
+    jellies[j].a += jellies[j].aStep;
+  }
   
+  // Rerender scene
   renderer.render(scene, camera)
 };
 
-
 // Generates a jellyfish based on the specified string (wish)
 const generateJelly = (string, scene) => {
-  const jellyGeometery = new THREE.SphereGeometry(40, 32, 32, 0, 6.3, 0, 1.7);
+  // generate unique hash for string, map to specific jelly properties
+  const jellyCode = hashFunc(string);
+  const jellyWidthSegments = Math.round(mapNumToRange(jellyCode[0], 1, 9, 5, 11));
+  const jellyHeightSegments = Math.round(mapNumToRange(jellyCode[1], 0, 9, 3, 8));
+  const jellyColor = Math.floor(mapNumToRange(jellyCode.substring(2, 4), 0, 99, 0.1, 0.9) * 16777215).toString(16);
+  const jellyAnimSpeed = mapNumToRange(jellyCode[4], 0, 9, 0.01, 0.09);
+
+  // create jelly shape and material
+  const jellyGeometery = new THREE.SphereGeometry(10, jellyWidthSegments, jellyHeightSegments, 0, 6.3, 0, 1.7);
   const jellyMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
+    color: `#${jellyColor}`,
     wireframe: true
   });
-  console.log(hashFunc(string));
+
+  // add jelly object to parent so we can move jelly without changing local coordinates and therefore messing up animation of jellies
   const jelly = new THREE.Mesh(jellyGeometery, jellyMaterial);
-  jelly.matrixAutoUpdate = true;
-  jelly.position.set(randomNum(-100, 100), randomNum(-100, 100), randomNum(-100, 100));
-  scene.add(jelly);
-  jellies.push(jelly);
+  const parent = new THREE.Object3D();
+  parent.position.set(randomNum(-200, 200), randomNum(-200, 200), randomNum(-200, 200));
+  parent.rotation.set(Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI, Math.random() * 2 * Math.PI);
+
+  // add jellies to scene
+  parent.add(jelly);
+	scene.add(parent);
+
+  // add jellies to list to track and aniamte
+  jellies.push({
+    jelly,
+    jellyParent: parent,
+    aStep: jellyAnimSpeed,
+    a: 0
+  });
 };
 
 // Get random num between min and max
@@ -166,22 +211,17 @@ const randomNum = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// Messing around with trying to get the hash function working
-const hashFunc = (string) => {
-  let hash = 0;
-  if(string.length === 0) return hash;
-  if(string.length < 10) {
-    for(let i = string.length; i < 10; i++) string += "_";
-  }
-  console.log(string);
-  for(let i = 0; i < string.length; i++) {
-    const char = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
-    hash = hash & hash;
-  }
-  return hash
+// Maps input number within specified range to specified output with specified range
+const mapNumToRange = (input, minInput, maxInput, minOutput, maxOutput) => {
+  return (input - minInput) * (maxOutput - minOutput) / (maxInput - minInput) + minOutput;
 };
 
+// Simple hash function
+const hashFunc = (s) => {
+  for(var i = 0, h = 0xdeadbeef; i < s.length; i++)
+      h = Math.imul(h ^ s.charCodeAt(i), 2654435761);
+  return ((h ^ h >>> 16) >>> 0).toString();
+};
 
 // Adds 20 wishes to the DB. Don't run this function unless needed
 // Must pass a database reference
@@ -208,7 +248,6 @@ const seedDB = (database) => {
   // Update DB
   database.ref().update(updates);
 };
-
 
 // Calls init function once DOM finishes loading
 window.onload = () => {
