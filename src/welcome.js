16ed421ba1f2848a0855ae7a08cddf45153b6847
AK44479 <ak4479@rit.edu>
2021-03-24 19:51:34 -0400

    Photo change, tweaked on welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -1,13 +1,22 @@
-
 "use strict";
 
 
 import * as THREE from '../build/three.module.js';
-import { EffectComposer } from './jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from './jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from './jsm/postprocessing/UnrealBloomPass.js';
-import { FilmPass } from './jsm/postprocessing/FilmPass.js';
-import { BokehPass } from './jsm/postprocessing/BokehPass.js';
+import {
+	EffectComposer
+} from './jsm/postprocessing/EffectComposer.js';
+import {
+	RenderPass
+} from './jsm/postprocessing/RenderPass.js';
+import {
+	UnrealBloomPass
+} from './jsm/postprocessing/UnrealBloomPass.js';
+import {
+	FilmPass
+} from './jsm/postprocessing/FilmPass.js';
+import {
+	BokehPass
+} from './jsm/postprocessing/BokehPass.js';
 
 var site = site || {};
 site.window = $(window);
@@ -39,38 +48,47 @@ $('#welcomescreen').on('mousemove', onDocumentMouseMove);
 init();
 animate();
 
-function init(){
-	
+function init() {
 
-	container = document.getElementById( 'container' );
+
+	container = document.getElementById('container');
 
 	// camera
 
-	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.set( 0, 0, 10 );
+	camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 1000);
+	camera.position.set(0, 0, 10);
+
+	// Listen for window resize and update renderer accordingly
+	window.addEventListener('resize', () => {
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	});
 
 	// scene
-
 	scene = new THREE.Scene();
 	// scene.background = new THREE.Color( 0x000000 );
 
 	// light
-    	const pointLight = new THREE.PointLight( 0xffffff, 1 );
-	camera.add( pointLight );
+	const pointLight = new THREE.PointLight(0xffffff, 1);
+	camera.add(pointLight);
 
-	const light = new THREE.DirectionalLight( 0xffffff );
-	light.position.set( 0, 0, 10.10 );
-	scene.add( light );
+	const light = new THREE.DirectionalLight(0xffffff);
+	light.position.set(0, 0, 10.10);
+	scene.add(light);
 
-	scene.add( new THREE.AmbientLight( 0xffffff ) );
+	scene.add(new THREE.AmbientLight(0xffffff));
 
 	// renderer
 
-	renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer = new THREE.WebGLRenderer({
+		antialias: true,
+		alpha: true
+	});
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.toneMapping = THREE.ReinhardToneMapping;
-	container.appendChild( renderer.domElement );	
+	container.appendChild(renderer.domElement);
 
 	// particle system
 	// createParticleSystem();
@@ -83,55 +101,57 @@ function init(){
 	// jelly
 
 	parent = new THREE.Object3D();
-	scene.add( parent );
+	scene.add(parent);
 
-    // TEST CUBE
-    addCubes(10);
+	// TEST CUBE
+	addCubes(10);
 
 }
 
 function animate() {
 
 
-    camera.position.x += ( (mouseX * .005) - camera.position.x ) * .05;
-    camera.position.y += ( -(mouseY * .005) - camera.position.y ) * .05;
+	camera.position.x += ((mouseX * .005) - camera.position.x) * .05;
+	camera.position.y += (-(mouseY * .005) - camera.position.y) * .05;
 
-    camera.lookAt( scene.position );
+	camera.lookAt(scene.position);
 
-    requestAnimationFrame(animate);
-	
-    // cube.rotation.y += 0.01;
+	requestAnimationFrame(animate);
 
-    renderer.render(scene, camera); 
+	// cube.rotation.y += 0.01;
 
-	
+	renderer.render(scene, camera);
+
+
 };
 
-function onDocumentMouseMove( event ) {
-    mouseX = ( event.clientX - windowHalfX ) / 2;
-    mouseY = ( event.clientY - windowHalfY ) / 2;
- }
+function onDocumentMouseMove(event) {
+	mouseX = (event.clientX - windowHalfX) / 2;
+	mouseY = (event.clientY - windowHalfY) / 2;
+}
 
- function addCubes(n){
+function addCubes(n) {
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshPhongMaterial( {color: 0xffffff, wireframe: false} );
-    let spread = 5;
-    let max = spread*2;
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	const material = new THREE.MeshPhongMaterial({
+		color: 0xffffff,
+		wireframe: false
+	});
+	let spread = 5;
+	let max = spread * 2;
 
 
-     for(let i=0; i<n; i++){
-        let cube = new THREE.Mesh( geometry, material );
-        if(n%1 == 0){
-            cube.position.x = ((Math.random()*max)-spread)+1;
-            cube.position.y = ((Math.random()*max)-spread)+1;
-            cube.position.z = ((Math.random()*max)-spread)+1;
-        }
-        else {
-            cube.position.x = Math.random()*-spread;
-            cube.position.y = Math.random()*-spread;
-        }
+	for (let i = 0; i < n; i++) {
+		let cube = new THREE.Mesh(geometry, material);
+		if (n % 1 == 0) {
+			cube.position.x = ((Math.random() * max) - spread) + 1;
+			cube.position.y = ((Math.random() * max) - spread) + 1;
+			cube.position.z = ((Math.random() * max) - spread) + 1;
+		} else {
+			cube.position.x = Math.random() * -spread;
+			cube.position.y = Math.random() * -spread;
+		}
 
-        scene.add( cube );
-     }
- }
+		scene.add(cube);
+	}
+}
