01cd88d1ef8bf357cff60035712a69fe1b4ae858
willmakesthings <wam5613@rit.edu>
2021-04-13 07:47:53 -0600

    Update Jelly Shader

--- src/jelly.js
+++ src/jelly.js
@@ -4,6 +4,8 @@
 
 import * as THREE from '../build/three.module.js';
 
+import Stats from './jsm/libs/stats.module.js';
+
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 import { EffectComposer } from './jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from './jsm/postprocessing/RenderPass.js';
@@ -14,40 +16,80 @@ import { BokehPass } from './jsm/postprocessing/BokehPass.js';
 let camera, scene, renderer, controls;
 let parent, geometry, mesh, subMesh, wireframeMesh;
 let container, composer;
-let jellyGeometry;
-let particles;
+let jellyGeometry, lines, materialShader, matShader, linematShader;
+let particles, stats;
 let center = new THREE.Vector3(0, 4000, 0);
 var timestep = 0;
 let a = 0;
 var clock = new THREE.Clock();
 let width = window.innerWidth;
 let height = window.innerHeight;
+let tentacleGeo, tentacle;
 const loader = new THREE.TextureLoader();
-
-
 
 const params = {
 
 	animate: true,
 	size: 12,
 	magnitude: 7,
-	segments: 16,
+	segments: 45,
 	wireframe: false,
-	focus: 100.0,
-	aperture: 2,
-	maxblur: 0.025
 
 };
 
-// const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+let texture = loader.load('../starmap_4k_print.jpeg');
+texture.mapping = THREE.EquirectangularReflectionMapping;
 
-const texture = loader.load( "../uv-lines.png" );
-texture.center.set = (0.5, 0.5);
+const mat = new THREE.MeshPhongMaterial({
+	color:0xe256cd,
+	transparent: true,
+	opacity: 0.25,
+	depthWrite: false,
+	reflectivity: 0.25,
+	envMap: texture
 
-const outerMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, transparent: true, opacity: 0.15, depthWrite: false})
-outerMaterial.side = THREE.DoubleSide;
-const innerMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, transparent: true, opacity: 0.25, depthWrite: false, map: texture})
-innerMaterial.side = THREE.DoubleSide;
+})
+
+mat.onBeforeCompile = (shader) => {
+    shader.uniforms.time = { value: 0}
+    shader.vertexShader = `
+        uniform float time;
+    ` + shader.vertexShader
+
+    const token = '#include <begin_vertex>'
+    const customTransform = `
+	vec3 transformed = vec3(position);
+	transformed.y = position.y + sin(position.y*0.40 + time*2.0);
+`
+
+    shader.vertexShader = shader.vertexShader.replace(token,customTransform)
+    matShader = shader
+}
+mat.side = THREE.DoubleSide;
+
+const lineMat = new THREE.LineBasicMaterial({color:0xffffff, transparent: true, opacity: 0.25})
+lineMat.onBeforeCompile = (shader) => {
+    shader.uniforms.time = { value: 0}
+    shader.vertexShader = `
+        uniform float time;
+    ` + shader.vertexShader
+
+    const token = '#include <begin_vertex>'
+    const customTransform = `
+	vec3 transformed = vec3(position);
+	transformed.x = position.x*0.95;
+	transformed.z = position.z*0.95;
+	transformed.y = position.y*0.65 
+	+ sin(position.y*0.40 + time*2.0);
+`
+    shader.vertexShader = shader.vertexShader.replace(token,customTransform)
+    linematShader = shader
+}
+
+// const outerMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, transparent: true, opacity: 0.15, depthWrite: false})
+// outerMaterial.side = THREE.DoubleSide;
+// const innerMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, transparent: true, opacity: 0.25, depthWrite: false, map: texture})
+// innerMaterial.side = THREE.DoubleSide;
 
 const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true, opacity: 1, transparent: true });
 
@@ -136,8 +178,8 @@ function addJelly(){
 	if (mesh !== undefined){
 			parent.remove( mesh );
 			mesh.geometry.dispose();
-			parent.remove( subMesh );
-			subMesh.geometry.dispose();
+			// parent.remove( subMesh );
+			// subMesh.geometry.dispose();
 	}
 
 	jellyGeometry = new THREE.SphereGeometry(15, params.segments, params.segments, 0, 6.283, 0, 1.7);
@@ -153,18 +195,69 @@ function addGeometry( geometry ) {
 		mesh = new THREE.Mesh( geometry, wireframeMaterial );
 		// subMesh = new THREE.Mesh( geometry, wireframeMaterial );
 	} else {
-		mesh = new THREE.Mesh( geometry, outerMaterial );
-		subMesh = new THREE.Mesh( geometry, innerMaterial );
+		mesh = new THREE.Mesh( geometry, mat );
+		let subJellyGeo = new THREE.SphereGeometry(14, params.segments, params.segments, 0, 6.283, 0, 1.9);
+		// subMesh = new THREE.Mesh( subJellyGeo, wireframeMaterial );
+		// scene.add(subMesh);
+		// subMesh.scale.set(0.98,0.65,0.98)
+
+		//Create lines out of mesh
+		// const edges = new THREE.EdgesGeometry( geometry );
+		// const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+		// parent.add( line );
+
+		// Create submesh lines
+		const vertex = mesh.geometry.attributes.position;
+		lines = [];
+		// < vertex.count
+		
+		for ( let i = 0; i < params.segments+1; i ++ ){
+			let temppoints = [];
+			for(let j=params.segments; j<(params.segments*params.segments); j+=(params.segments+1)){
+				// temppoints.push(j+i);
+				var vec = new THREE.Vector3();
+				vec.fromBufferAttribute(vertex, i+j)
+				temppoints.push(vec);
+			}
+			// console.log(temppoints);
+			const linegeo = new THREE.BufferGeometry().setFromPoints( temppoints );
+			lines.push( new THREE.Line( linegeo, lineMat));
+		}
+
 		// wireframeMesh = new THREE.Mesh( geometry, wireframeMaterial );
 		// mesh.add( wireframeMesh );
 	}
 
 	mesh.depthWrite = false;
-	subMesh.depthWrite = false;
+	// subMesh.depthWrite = false;
+	
+	for(let i=0; i<lines.length; i++){
+		mesh.add(lines[i]);
+	}
 	parent.add( mesh );
-	parent.add( subMesh );
+	mesh.position.y = -10
+	// parent.add( subMesh );
 	
 
+}
+
+function addTentacles(){
+
+	const MAX_POINTS = 550;
+
+	tentacleGeo = new THREE.BufferGeometry();
+	const positions = new Float32Array( MAX_POINTS * 3 ); //3 Vertices per point
+	tentacleGeo.setAttribute( 'position', new THREE.BufferAttribute(positions, 3) );
+	const tentacleMat = new THREE.LineBasicMaterial( { color: 0xffffff } );
+
+	tentacle = new THREE.Line( tentacleGeo, tentacleMat );
+	scene.add(tentacle);
+
+	let numTentacles = 5;
+
+	// for(let i=0; i<numTentacles; i++){
+	// 	scene.add(new tentacle.clone);
+	// }
 }
 
 init();
@@ -178,7 +271,7 @@ function init(){
 	// camera
 
 	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.set( 0, 0, 300 );
+	camera.position.set( 0, 0, 50 );
 
 	// scene
 
@@ -187,11 +280,11 @@ function init(){
 
 	// light
 
-	const light = new THREE.DirectionalLight( 0xffffff );
-	light.position.set( 0, 0, 0.10 );
-	scene.add( light );
+	// const light = new THREE.DirectionalLight( 0xffffff, 1 );
+	// light.position.set( 0, 100, 0 );
+	// scene.add( light );
 
-	scene.add( new THREE.AmbientLight( 0x404040 ) );
+	scene.add( new THREE.AmbientLight( 0xffffff, 0.25 ) );
 
 	// const pointLight = new THREE.PointLight( 0xffffff, 1 );
 	// camera.add( pointLight );
@@ -205,7 +298,7 @@ function init(){
 	container.appendChild( renderer.domElement );	
 
 	// particle system
-	// createParticleSystem();
+	createParticleSystem();
 
 	// controls
 
@@ -217,13 +310,13 @@ function init(){
 	parent = new THREE.Object3D();
 	scene.add( parent );
 
-	// Establish Wireframe Clipping
+	// Establish Wireframe Clipping 
 	// localPlane = new THREE.Plane( new THREE.Vector3( 0, 1, 0 ), 0 );
 	// wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true, clippingPlanes: [localPlane] } );
 
 	addJelly();
-	subMesh.scale.set(0.98,0.65,0.98)
-
+	// subMesh.scale.set(0.98,0.65,0.98)
+	addTentacles();
 
 	// GUI
 	const gui = new dat.GUI();
@@ -236,6 +329,9 @@ function init(){
 	gui.add( params, 'wireframe' ).onChange( function () {
 		addJelly();
 	} );
+
+	stats = new Stats();
+	document.body.appendChild( stats.dom );
 
 	// Render Pass Effects
 	const renderScene = new RenderPass( scene, camera );
@@ -253,12 +349,11 @@ function init(){
 		false,  // grayscale
 	);
 	filmPass.renderToScreen = true;
-	
-	const bokehPass = new BokehPass( scene, camera, {
-		focus: params.focus,
-		aperture: params.aperture * 0.00001,
-		maxblur: params.maxblur,
 
+	const bokehPass = new BokehPass( scene, camera, {
+		focus: 50,
+		aperture: 2 * 0.00001,
+		maxblur: 0.25,
 		width: width,
 		height: height
 	} );
@@ -269,58 +364,114 @@ function init(){
 	composer = new EffectComposer( renderer );
 	composer.addPass( renderScene );
 	composer.addPass( bloomPass );
-	composer.addPass(filmPass);
+	// composer.addPass(filmPass);
 	// composer.addPass(bokehPass);
 
 	// renderer.clippingPlanes = [ localPlane ]; // GUI sets it to globalPlanes
 	// renderer.localClippingEnabled = true;
 
 	//CCapture
-	capturer = new CCapture( { format: 'png', quality: 100, timeLimit: 24, autoSaveTime: 0 } );
-	capturer.start();
-
-	
+	// capturer = new CCapture( { format: 'png', quality: 100, timeLimit: 24, autoSaveTime: 0 } );
+	// capturer.start();
 
 }
 
+function updateTentacles(){
+	const positions = tentacle.geometry.attributes.position.array;
+	
+	const shiftRight = (collection, value) => {
+		for (let i = collection.length - 1; i > 0; i--) {
+		  collection[i] = collection[i - 1]; // Shift right
+		}
+		collection[0] = value; // Place new value at head
+		return collection;
+	  }
 
+	  shiftRight(positions, parent.position.z);
+	  shiftRight(positions, parent.position.y);
+	  shiftRight(positions, parent.position.x);
 
-function animate() {
+	tentacle.geometry.attributes.position.needsUpdate = true; // required after the first render
+
+}
+
+function animate(time) {
 	var delta = clock.getDelta();
 
+	stats.update();
+
     requestAnimationFrame(animate);
-	
-	// particles.rotation.y+=0.001;
+
+	if(params.animate){
+		if(matShader) matShader.uniforms.time.value = time/2000;
+		if(linematShader) linematShader.uniforms.time.value = time/2000;
+		particles.rotation.y+=0.001;
+		
+	}
+	// parent.rotateX((Math.PI / 1000) * Math.random());
+    // parent.rotateZ((Math.PI / 1000) * Math.random());
+
+	// parent.translateY(0.3);
+    // if(parent.position.x < -1300 || parent.position.x > 1300) {
+    //   parent.position.x = THREE.Math.clamp(parent.position.x, -1300, 1300);
+    //   parent.rotateX(Math.PI);
+    // }
+    // if(parent.position.y < -1300 || parent.position.y > 1300) {
+    //   parent.position.y = THREE.Math.clamp(parent.position.y, -1300, 1300);
+    //   parent.rotateY(Math.PI);
+    // }
+    // if(parent.position.z < -1300 || parent.position.z > 1300) {
+    //   parent.position.z = THREE.Math.clamp(parent.position.z, -1300, 1300);
+    //   parent.rotateZ(Math.PI);
+    // }
+
+
+	updateTentacles();
+
+	// controls.target = parent.position;
+	// controls.update();
+
+
 	// subMesh.scale = 0.785;
 	// localPlane.constant = a * -25;
 
-    if ( mesh.isMesh && params.animate) {
+    // if ( mesh.isMesh && params.animate) {
    
-        const position = mesh.geometry.attributes.position;
-        const vector = new THREE.Vector3();
+    //     const position = mesh.geometry.attributes.position;
+    //     const vector = new THREE.Vector3();
 
-        for ( let i = 0; i < position.count; i ++ ){
-            // console.log("yes");
-           vector.fromBufferAttribute( position, i );
-           vector.applyMatrix4( mesh.matrixWorld );
-            let size = params.size;
-            let magnitude = params.magnitude;
-            let dist = new THREE.Vector3(vector.x, vector.y, vector.z).sub(center);
+    //     for ( let i = 0; i < position.count; i ++ ){
+    //         // console.log("yes");
+    //        vector.fromBufferAttribute( position, i );
+    //        vector.applyMatrix4( mesh.matrixWorld );
+    //         let size = params.size;
+    //         let magnitude = params.magnitude;
+    //         let dist = new THREE.Vector3(vector.x, vector.y, vector.z).sub(center);
 
-            vector.normalize();
-            vector.multiplyScalar(40 + Math.sin(dist.length()/-size + a) * magnitude);
-            position.setXYZ(i, vector.x, vector.y, vector.z);
+    //         vector.normalize();
+    //         vector.multiplyScalar(40 + Math.sin(dist.length()/-size + a) * magnitude);
+    //         position.setXYZ(i, vector.x, vector.y, vector.z);
+	// 		// for(let j=0; j<lines.length;j++){
+	// 		// 	// console.log(lines[j].geometry.attributes.position)
+	// 		// 	lines[j].geometry.attributes.position.setXYZ(i, vector.x, vector.y, vector.z)
+	// 		// }
 
-        }
-        position.needsUpdate = true;
-        mesh.geometry.verticesNeedUpdate = true;
-        mesh.geometry.normalsNeedUpdate = true;
-        mesh.geometry.computeVertexNormals();
-        mesh.geometry.computeFaceNormals();
+    //     }
+    //     position.needsUpdate = true;
+    //     mesh.geometry.verticesNeedUpdate = true;
+    //     mesh.geometry.normalsNeedUpdate = true;
+    //     mesh.geometry.computeVertexNormals();
+    //     mesh.geometry.computeFaceNormals();
+	// 	for(let j=0; j<lines.length;j++){
+	// 		lines[j].geometry.verticesNeedUpdate = true;
+	// 		lines[j].geometry.normalsNeedUpdate = true;
+	// 		lines[j].geometry.computeVertexNormals();
+    //     	lines[j].geometry.computeFaceNormals();
+	// 	}
         
 		
-    }
-	a += 0.05;
+    // }
+	// a += 0.025;
 	// if(wireframeMesh.material.opacity > 0){
 	// wireframeMesh.material.opacity = wireframeMesh.material.opacity - (a*0.0005);
 	// }
@@ -334,7 +485,7 @@ function animate() {
 	// composer.render(delta);
 	
     composer.render(scene, camera); 
-	capturer.capture( renderer.domElement );
+	// capturer.capture( renderer.domElement );
 
 	
 };
