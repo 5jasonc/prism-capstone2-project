6ce228906b2bc3648aae6d6f428cf518fae58c00
Gabriel Veit <gkv8601@rit.edu>
2021-04-14 22:33:50 -0400

    Merge branch 'main' of https://github.com/5jasonc/prism-capstone2-project into main
    # Conflicts:
    #	aboutus.html

--- src/welcome.js
+++ src/welcome.js
@@ -2,6 +2,20 @@
 
 
 import * as THREE from '../build/three.module.js';
+import Stats from './jsm/libs/stats.module.js';
+import {
+	GUI
+} from './jsm/libs/dat.gui.module.js';
+import {
+	Water
+} from './jsm/objects/Water.js';
+import {
+	Sky
+} from './jsm/objects/Sky.js';
+import {
+	OrbitControls
+} from './jsm/controls/OrbitControls.js';
+
 
 import {
 	EffectComposer
@@ -25,15 +39,21 @@ site.document = $(document);
 site.Width = site.window.width();
 site.Height = site.window.height();
 
-let container;
+let container, stats;
 let camera, scene, renderer;
 let mouseX = 0;
 let mouseY = 0;
 var windowHalfX = site.Width / 2;
 var windowHalfY = site.Height / 2;
 let makewish = false;
+let controls, water, sun, mesh;
 
-const { autoPlay, Easing, onTick, Tween } = TWEEN;
+const {
+	autoPlay,
+	Easing,
+	onTick,
+	Tween
+} = TWEEN;
 
 const params = {
 
@@ -51,8 +71,6 @@ const params = {
 $('#welcomescreen').on('mousemove', onDocumentMouseMove);
 
 function init() {
-
-
 	container = document.getElementById('container');
 
 	// camera
@@ -82,72 +100,165 @@ function init() {
 	scene.add(new THREE.AmbientLight(0xffffff));
 
 	// renderer
-
 	renderer = new THREE.WebGLRenderer({
 		antialias: true,
 		alpha: true
 	});
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.toneMapping = THREE.ReinhardToneMapping;
+	renderer.NoToneMapping = THREE.ACESFilmicToneMapping;
 	container.appendChild(renderer.domElement);
 
 	// particle system
 	// createParticleSystem();
 
 	// controls
+	//controls = new OrbitControls(camera, renderer.domElement)
+	//controls.listenToKeyEvents( window ); // optional
 
-	// controls = new OrbitControls(camera, renderer.domElement)
-	// controls.listenToKeyEvents( window ); // optional
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.maxPolarAngle = Math.PI * 0.495;
+	controls.target.set(0, 10, 0);
+	controls.minDistance = 40.0;
+	controls.maxDistance = 200.0;
+	controls.update();
 
 	// jelly
+
+	sun = new THREE.Vector3();
+	// Water
+	const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+	water = new Water(
+		waterGeometry, {
+			textureWidth: 560,
+			textureHeight: 560,
+			waterNormals: new THREE.TextureLoader().load('../waternormals.jpeg', function (texture) {
+
+				texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+			}),
+			sunDirection: new THREE.Vector3(),
+			sunColor: 0xffffff,
+			waterColor: 0xaccde8,
+			distortionScale: 6,
+			fog: scene.fog !== undefined
+		}
+	);
+
+	water.rotation.x = -Math.PI / 2;
+	scene.add(water);
 
 	parent = new THREE.Object3D();
 	scene.add(parent);
 
+	// Skybox
+	const sky = new Sky();
+	sky.scale.setScalar(10000);
+	scene.add(sky);
+
+	const skyUniforms = sky.material.uniforms;
+
+	skyUniforms['turbidity'].value = 20;
+	skyUniforms['rayleigh'].value = 0.036;
+	skyUniforms['mieCoefficient'].value = 0;
+	skyUniforms['mieDirectionalG'].value = 1;
+
+	const parameters = {
+		inclination: 0.4857,
+		azimuth: 0.252,
+		exposure: 0.1389
+	};
+
+	const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+	function updateSun() {
+
+		const theta = Math.PI * (parameters.inclination - 0.5);
+		const phi = 2 * Math.PI * (parameters.azimuth - 0.5);
+
+		sun.x = Math.cos(phi);
+		sun.y = Math.sin(phi) * Math.sin(theta);
+		sun.z = Math.sin(phi) * Math.cos(theta);
+
+		sky.material.uniforms['sunPosition'].value.copy(sun);
+		water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+
+		scene.environment = pmremGenerator.fromScene(sky).texture;
+		//
+
+		controls = new OrbitControls(camera, renderer.domElement);
+		controls.maxPolarAngle = Math.PI * 0.495;
+		controls.target.set(0, 10, 0);
+		controls.minDistance = 40.0;
+		controls.maxDistance = 200.0;
+		controls.update();
+	}
+
+	updateSun();
+
 	// TEST CUBE
-	addCubes(10);
+	//addCubes(10);
+
+	window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
 function animate() {
 
 	// CAMERA ANIMATION ON MOUSE MOVE
-	if(makewish==false){
-	camera.position.x += ((mouseX * .005) - camera.position.x) * .05;
-	camera.position.y += (-(mouseY * .005) - camera.position.y) * .05;
+	// if (makewish == false) {
+	// 	camera.position.x += ((mouseX * .005) - camera.position.x) * .05;
+	// 	camera.position.y += (-(mouseY * .005) - camera.position.y) * .05;
 
-	camera.lookAt(scene.position);
-	}
+	// 	camera.lookAt(scene.position);
+	// }
 
 	TWEEN.update();
 	requestAnimationFrame(animate);
+	render();
+}
+
+function render() {
+	const time = performance.now() * 0.001;
+
+	water.material.uniforms['time'].value += 1.0 / 60.0;
 
 	renderer.render(scene, camera);
-
 }
 
 $('.welcometxt').click(wish);
-$('.controlscontainer').delay( 800 ).fadeIn();
+$('.controlscontainer').delay(800).fadeIn();
 
-function wish(){
+function wish() {
 
-	makewish=true;
+	makewish = true;
 
-      new TWEEN.Tween(camera.position)
-      .to({'y': 40}, 5000)
-      .easing(Easing.Circular.InOut)
-      .start();
+	new TWEEN.Tween(camera.position)
+		.to({
+			'y': 100
+		}, 5000)
+		.easing(Easing.Circular.InOut)
+		.start();
 
-	  $('#welcomescreen').fadeOut(1000, 0);
-	  $('#container').attr('style', 'background-color: black');
-	  $('.starveiw').fadeIn(3000);
-	  
-	  setTimeout(
-        function () {
-            $("#storycover").fadeOut(2000);
-        }, 7000
-    );
+	$('#welcomescreen').fadeOut(1000, 0);
+	$('#container').attr('style', 'background-color: black');
+	$('.starveiw').fadeIn(3000);
+	$('#container').fadeOut(5000,0)
+
+	setTimeout(
+		function () {
+			$("#storycover").fadeOut(2000);
+		}, 7000
+	);
 }
 
 function onDocumentMouseMove(event) {
@@ -182,7 +293,8 @@ function addCubes(n) {
 	}
 }
 
+
 window.onload = () => {
-  init();
-  animate();
+	init();
+	animate();
 };
